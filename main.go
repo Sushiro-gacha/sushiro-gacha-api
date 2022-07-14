9ccf42b4c0da02b05e8bf23c60ba15135213d080
shunhamm <shun.022885@gmail.com>
2022-07-14 17:37:42 +0900

    Edit main.go. - change root path

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 }
 
 func StartMainServer() {
-	http.HandleFunc("/home/price/", controller.GathaPrice)
+	http.HandleFunc("/gacha/price/", controller.GathaPrice)
 	//http.HandleFunc("/home/calorie/", controller.GachaCalories)
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
