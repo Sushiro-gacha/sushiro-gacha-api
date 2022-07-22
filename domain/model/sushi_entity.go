91e7a871b8fbddab06e677565bc0d02196d53189
shunhamm <shun.022885@gmail.com>
2022-07-22 14:58:21 +0900

    - Add sushi_entity for input data. - Edit sushi_repository. Func return_sushi get json input and return go struct

---
+++ domain/model/sushi_entity.go
@@ -0,0 +1,9 @@
+package model
+
+type SushiEntity struct {
+	Id       int
+	Category string
+	Name     string
+	Price    int
+	Calories int
+}
--- domain/repository/sushi_repository.go
+++ domain/repository/sushi_repository.go
@@ -1,1 +1,24 @@
 package repository
+
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/Sushiro-gacha/sushiro-gacha-api/domain/model"
+)
+
+func return_sushi() model.SushiEntity {
+	sushi_string := `{
+			"id":       1,
+			"category": "nigiri",
+			"name":     "maguro",
+			"proce":    100,
+			"calories": 100,
+		}`
+	var sushi model.SushiEntity
+	if err := json.Unmarshal([]byte(sushi_string), &sushi); err != nil {
+		log.Fatal(err)
+	}
+
+	return sushi
+}
