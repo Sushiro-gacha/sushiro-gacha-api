f94ef9fd32fdef6fb8daa887514918052aae0e6f
shunhamm <shun.022885@gmail.com>
2022-08-22 12:59:58 +0900

    fix some issues in the comment

--- app/controller/gacha_controller.go
+++ app/controller/gacha_controller.go
@@ -4,25 +4,20 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/Sushiro-gacha/sushiro-gacha-api/domain/service"
 )
 
+const DEFAULT_BUDGET int = 1000
+
 func GachaPrice(w http.ResponseWriter, r *http.Request) {
-	sushiList := service.FetchSushiData()
-	queryMap := r.URL.Query()
-	value, _ := strconv.Atoi(queryMap["value"][0])
-	sushiPriceList := service.ChoiseSushiPriceCondition(sushiList, value)
+	sushiPriceList := service.FetchGachaResult(w, r)
 	sushiJson, err := json.Marshal(sushiPriceList)
+	// output: [4/8]0xc0000165d0
+	println(sushiJson)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(sushiJson)
 }
-
-// func GachaCalories(w http.ResponseWriter, r *http.Request) Calorie {
-
-// }
--- domain/service/gacha_service.go
+++ domain/service/gacha_service.go
@@ -1,20 +1,50 @@
 package service
 
 import (
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+
 	"github.com/Sushiro-gacha/sushiro-gacha-api/domain/model"
 	"github.com/Sushiro-gacha/sushiro-gacha-api/domain/repository"
 )
 
-func ChoiseSushiPriceCondition(sushiList []model.Sushi, maxPrice int) []model.Sushi {
+func FetchGachaResult(w http.ResponseWriter, r *http.Request) []model.Sushi {
+	var totalBudget int
+	sushiList := fetchSushiData()
+	queryMap, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if queryMap["value"] != nil {
+		totalBudget, err = strconv.Atoi(queryMap["value"][0])
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	//ここまで通ってる
+	sushiPriceList := choiseSushiPriceCondition(sushiList, totalBudget)
+	// output: [0/0]0x0
+	println(sushiPriceList)
+	return sushiPriceList
+}
+
+func choiseSushiPriceCondition(sushiList []model.Sushi, totalBudget int) []model.Sushi {
 	var resultSushiList []model.Sushi
-	for sumPrice := 0; sumPrice < maxPrice; {
-		resultSushiList = append(resultSushiList, sushiList[randomNumber])
-		sumPrice = sumPrice + sushiList[randomNumber].Price
+	for sumPrice := 0; sumPrice > totalBudget; {
+		randomNumber := rand.Intn(len(sushiList)) + 1
+		sushiChild := sushiList[randomNumber]
+		sumPrice = sumPrice + sushiChild.Price
+		if sumPrice < totalBudget {
+			resultSushiList = append(resultSushiList, sushiChild)
+		}
 	}
 	return resultSushiList
 }
 
-func FetchSushiData() []model.Sushi {
+func fetchSushiData() []model.Sushi {
 	var sushiList []model.Sushi
 	sushiEntityList := repository.FetchSushiData()
 	for _, entity := range sushiEntityList {
