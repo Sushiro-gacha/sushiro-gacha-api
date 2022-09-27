884ea2604f74eb98d4bdd367fc830ded282d6553
shunhamm <shun.022885@gmail.com>
2022-09-28 00:18:37 +0900

    success connect to mysql docker

--- main.go
+++ main.go
@@ -6,32 +6,31 @@ import (
 	"net/http"
 
 	"github.com/Sushiro-gacha/sushiro-gacha-api/app/controller"
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jinzhu/gorm"
+	database "github.com/Sushiro-gacha/sushiro-gacha-api/db"
 )
 
 func main() {
-	_, err := sqlConnect()
-	if err != nil {
-		panic(err.Error())
-	} else {
-		fmt.Println("DB接続成功")
-	}
 
+	db := database.Connect()
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}()
+
+	err := db.Ping()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	} else {
+		fmt.Println("データベース接続成功")
+	}
 	// StartMainServer()
 }
+
 func StartMainServer() {
 	http.HandleFunc("/gacha/price", controller.GachaPrice)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-}
-
-func sqlConnect() (database *gorm.DB, err error) {
-	DBMS := "mysql"
-	USER := "docker"
-	PASS := "docker"
-	PROTOCOL := "tcp(localhost:3306)"
-	DBNAME := "test_database"
-
-	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
-	return gorm.Open(DBMS, CONNECT)
 }
